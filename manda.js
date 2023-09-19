67678340df4555a05a257d414bea839b1c4bc7a6
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2023-09-19 17:08:20 -0300

    feat[]: added new test

---
+++ manda.js
@@ -0,0 +1,46 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+var express = require('express');
+
+var app = express();
+
+// Set the region 
+AWS.config.update({region: 'REGION'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var params = {
+   // Remove DelaySeconds parameter and value for FIFO queues
+  DelaySeconds: 10,
+  MessageAttributes: {
+    "Title": {
+      DataType: "String",
+      StringValue: "The Whistler"
+    },
+    "Author": {
+      DataType: "String",
+      StringValue: "John Grisham"
+    },
+    "WeeksOn": {
+      DataType: "Number",
+      StringValue: "6"
+    }
+  },
+  MessageBody: "Information about current NY Times fiction bestseller for week of 12/11/2016.",
+  // MessageDeduplicationId: "TheWhistler",  // Required for FIFO queues
+  // MessageGroupId: "Group1",  // Required for FIFO queues
+  QueueUrl: "https://sqs.us-east-1.amazonaws.com/051736337328/novo-teste"
+};
+
+sqs.sendMessage(params, function(err, data) {
+  if (err) {
+    console.log("Error", err);
+  } else {
+    console.log("Success", data.MessageId);
+  }
+});
+
+app.listen(3006, () => {
+    console.log("http://localhost:3006")
+})
---
+++ recebe.js
@@ -0,0 +1,48 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+var express = require('express');
+
+var app = express();
+// Set the region
+AWS.config.update({region: 'REGION'});
+
+// Create an SQS service object
+var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+var queueURL = "https://sqs.us-east-1.amazonaws.com/051736337328/novo-teste";
+
+var params = {
+ AttributeNames: [
+    "SentTimestamp"
+ ],
+ MaxNumberOfMessages: 10,
+ MessageAttributeNames: [
+    "All"
+ ],
+ QueueUrl: queueURL,
+ VisibilityTimeout: 20,
+ WaitTimeSeconds: 0
+};
+
+sqs.receiveMessage(params, function(err, data) {
+  if (err) {
+    console.log("Receive Error", err);
+  } else if (data.Messages) {
+    var deleteParams = {
+      QueueUrl: queueURL,
+      ReceiptHandle: data.Messages[0].ReceiptHandle
+    };
+    sqs.deleteMessage(deleteParams, function(err, data) {
+      if (err) {
+        console.log("Delete Error", err);
+      } else {
+        console.log("Message Deleted", data);
+      }
+    });
+  }
+});
+
+app.listen(3007, () => {
+    console.log("http://localhost:3007")
+})
